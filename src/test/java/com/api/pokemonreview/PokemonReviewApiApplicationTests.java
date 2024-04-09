f3e814adf598b6669baa940217ed923f506ceca8
rikcav <henriquearau000@gmail.com>
2024-04-09 12:55:47 -0300

    feat: initial test

---
+++ src/test/java/com/api/pokemonreview/PokemonReviewApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.pokemonreview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokemonReviewApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
