9e090da0f5571c29290a240172c7094406f29b8c
rikcav <henriquearau000@gmail.com>
2024-04-09 12:57:33 -0300

    feat: initial point of api

---
+++ src/main/java/com/api/pokemonreview/PokemonReviewApiApplication.java
@@ -0,0 +1,13 @@
+package com.api.pokemonreview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokemonReviewApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PokemonReviewApiApplication.class, args);
+    }
+
+}
