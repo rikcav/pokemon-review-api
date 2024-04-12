885407e7fec8ac7b7a41e8e3a6aa4a6b3d725b22
rikcav <henriquearau000@gmail.com>
2024-04-12 16:44:24 -0300

    feat: added user and role entities

---
+++ src/main/java/com/api/pokemonreview/models/Role.java
@@ -0,0 +1,16 @@
+package com.api.pokemonreview.models;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "roles")
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String name;
+}
