3e9d1f1af0369be52c4951166a0e15aa803730db
rikcav <henriquearau000@gmail.com>
2024-04-14 17:38:27 -0300

    fix: authentication response dto

---
+++ src/main/java/com/api/pokemonreview/dtos/AuthResponseDTO.java
@@ -0,0 +1,13 @@
+package com.api.pokemonreview.dtos;
+
+import lombok.Data;
+
+@Data
+public class AuthResponseDTO {
+    private String accessToken;
+    private String tokenType = "Bearer ";
+
+    public AuthResponseDTO(String accessToken) {
+        this.accessToken = accessToken;
+    }
+}
