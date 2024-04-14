81da5eb1a520a14193f9a1940056c5291a71d78e
rikcav <henriquearau000@gmail.com>
2024-04-14 10:50:33 -0300

    feat: user repository

---
+++ src/main/java/com/api/pokemonreview/repositories/UserRepository.java
@@ -0,0 +1,12 @@
+package com.api.pokemonreview.repositories;
+
+import com.api.pokemonreview.models.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<UserEntity, Integer> {
+    Optional<UserEntity> findByUsername(String username);
+
+    Boolean existsByUsername(String username);
+}
