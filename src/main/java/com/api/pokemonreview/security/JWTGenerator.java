ba44733744913b4d6d9ed187c693494f91975341
rikcav <henriquearau000@gmail.com>
2024-04-14 16:43:33 -0300

    feat: json web token generator

---
+++ src/main/java/com/api/pokemonreview/security/JWTGenerator.java
@@ -0,0 +1,49 @@
+package com.api.pokemonreview.security;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.security.authentication.AuthenticationCredentialsNotFoundException;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+import static com.api.pokemonreview.security.SecurityConstants.JWT_EXPIRATION;
+import static com.api.pokemonreview.security.SecurityConstants.JWT_SECRET;
+
+@Component
+public class JWTGenerator {
+    //TODO refactor this class using auth0 jwt
+    public String generateToken(Authentication authentication) {
+        String username = authentication.getName();
+        Date currentDate = new Date();
+        Date expirationDate = new Date(currentDate.getTime() + JWT_EXPIRATION);
+
+        return Jwts.builder()
+                .setSubject(username)
+                .setIssuedAt(currentDate)
+                .setExpiration(expirationDate)
+                .signWith(SignatureAlgorithm.ES512, JWT_SECRET)
+                .compact();
+    }
+
+    public String getUsername(String token) {
+        Claims claims = Jwts.parser()
+                .setSigningKey(JWT_SECRET)
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+
+        return claims.getSubject();
+    }
+
+    public boolean validateToken(String token) {
+        try {
+            Jwts.parser().setSigningKey(JWT_SECRET).build().parseClaimsJws(token);
+            return true;
+        } catch (Exception e) {
+            throw new AuthenticationCredentialsNotFoundException("JWT was expired or incorrect.");
+        }
+    }
+}
