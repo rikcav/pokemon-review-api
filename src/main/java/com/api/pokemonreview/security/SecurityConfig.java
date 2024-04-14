36cee780f0e916955b3713e55b01acfd90d3c9f7
rikcav <henriquearau000@gmail.com>
2024-04-14 16:42:42 -0300

    feat: added jwt auth entrypoint dependency injection

--- src/main/java/com/api/pokemonreview/security/SecurityConfig.java
+++ src/main/java/com/api/pokemonreview/security/SecurityConfig.java
@@ -9,6 +9,7 @@ import org.springframework.security.config.annotation.authentication.configurati
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -17,16 +18,20 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableWebSecurity
 public class SecurityConfig {
     private final CustomUserDetailsService userDetailsService;
+    private final JWTAuthEntrypoint jwtAuthEntrypoint;
 
     @Autowired
-    public SecurityConfig(CustomUserDetailsService userDetailsService) {
+    public SecurityConfig(CustomUserDetailsService userDetailsService, JWTAuthEntrypoint jwtAuthEntrypoint) {
         this.userDetailsService = userDetailsService;
+        this.jwtAuthEntrypoint = jwtAuthEntrypoint;
     }
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(AbstractHttpConfigurer::disable)
+                .exceptionHandling(configurer -> configurer.authenticationEntryPoint(jwtAuthEntrypoint))
+                .sessionManagement(configurer -> configurer.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(authorize -> authorize
                         .requestMatchers("/api/auth/**").permitAll() // Can permit users by specific role
                         .anyRequest().authenticated()
