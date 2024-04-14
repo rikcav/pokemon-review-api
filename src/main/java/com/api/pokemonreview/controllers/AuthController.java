383c4b003cd012b5a35737a747310fefe0649ba5
rikcav <henriquearau000@gmail.com>
2024-04-14 14:07:01 -0300

    feat: authentication controller

---
+++ src/main/java/com/api/pokemonreview/controllers/AuthController.java
@@ -0,0 +1,59 @@
+package com.api.pokemonreview.controllers;
+
+import com.api.pokemonreview.dtos.RegisterDTO;
+import com.api.pokemonreview.models.Role;
+import com.api.pokemonreview.models.UserEntity;
+import com.api.pokemonreview.repositories.RoleRepository;
+import com.api.pokemonreview.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collections;
+
+@RestController
+@RequestMapping("/api/auth/")
+public class AuthController {
+    //TODO refactor this code into controller and service classes
+    private AuthenticationManager authenticationManager;
+    private UserRepository userRepository;
+    private RoleRepository roleRepository;
+    private PasswordEncoder passwordEncoder;
+
+    @Autowired
+    public AuthController(
+            AuthenticationManager authenticationManager,
+            UserRepository userRepository,
+            RoleRepository roleRepository,
+            PasswordEncoder passwordEncoder) {
+        this.authenticationManager = authenticationManager;
+        this.userRepository = userRepository;
+        this.roleRepository = roleRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @PostMapping("register")
+    public ResponseEntity<String> register(@RequestBody RegisterDTO registerDTO) {
+        if (userRepository.existsByUsername(registerDTO.getUsername())) {
+            return new ResponseEntity<>("That username is taken.", HttpStatus.BAD_REQUEST);
+        }
+
+        UserEntity user = new UserEntity();
+        user.setUsername(registerDTO.getUsername());
+        user.setPassword(passwordEncoder.encode(registerDTO.getPassword()));
+
+        Role roles = roleRepository.findByName("USER")
+                .orElseThrow(() -> new RuntimeException("Role not found."));
+
+        user.setRoles(Collections.singletonList(roles));
+        userRepository.save(user);
+
+        return new ResponseEntity<>("User registered successfully.", HttpStatus.OK);
+    }
+}
