1a5a0f0c0216c60c22adb0cebf2b2a80dde7680b
rikcav <henriquearau000@gmail.com>
2024-04-14 16:43:05 -0300

    feat: added security constants for jwt generator

---
+++ src/main/java/com/api/pokemonreview/security/SecurityConstants.java
@@ -0,0 +1,6 @@
+package com.api.pokemonreview.security;
+
+public class SecurityConstants {
+    public static final long JWT_EXPIRATION = 70_000;
+    public static final String JWT_SECRET = "my-secret";
+}
