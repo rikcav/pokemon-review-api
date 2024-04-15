f86e664c17f553fb72cf83b5741ed25f23c7f013
rikcav <henriquearau000@gmail.com>
2024-04-15 09:38:41 -0300

    fix: correct method return to AuthResponseDTO

---
+++ src/main/java/com/api/pokemonreview/services/impl/AuthServiceImpl.java
@@ -0,0 +1,55 @@
+package com.api.pokemonreview.services.impl;
+
+import com.api.pokemonreview.dtos.AuthResponseDTO;
+import com.api.pokemonreview.dtos.LoginDTO;
+import com.api.pokemonreview.dtos.RegisterDTO;
+import com.api.pokemonreview.repositories.RoleRepository;
+import com.api.pokemonreview.repositories.UserRepository;
+import com.api.pokemonreview.security.JWTGenerator;
+import com.api.pokemonreview.services.IAuthService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthServiceImpl implements IAuthService {
+    private final AuthenticationManager authenticationManager;
+    private final UserRepository userRepository;
+    private final RoleRepository roleRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final JWTGenerator jwtGenerator;
+
+    @Autowired
+    public AuthServiceImpl(
+            AuthenticationManager authenticationManager,
+            UserRepository userRepository,
+            RoleRepository roleRepository,
+            PasswordEncoder passwordEncoder,
+            JWTGenerator jwtGenerator) {
+        this.authenticationManager = authenticationManager;
+        this.userRepository = userRepository;
+        this.roleRepository = roleRepository;
+        this.passwordEncoder = passwordEncoder;
+        this.jwtGenerator = jwtGenerator;
+    }
+
+    @Override
+    public AuthResponseDTO login(LoginDTO loginDTO) {
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(loginDTO.getUsername(), loginDTO.getPassword()));
+
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        String token = jwtGenerator.generateToken(authentication);
+
+        return new AuthResponseDTO(token);
+    }
+
+    @Override
+    public String register(RegisterDTO registerDTO) {
+        return "";
+    }
+}
