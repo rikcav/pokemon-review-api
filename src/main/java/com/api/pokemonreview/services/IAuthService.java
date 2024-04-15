52aba3a8c388d6731ef2a44142b57aecf6828bb2
rikcav <henriquearau000@gmail.com>
2024-04-15 09:39:12 -0300

    fix: correct method return to AuthResponseDTO

--- src/main/java/com/api/pokemonreview/services/IAuthService.java
+++ src/main/java/com/api/pokemonreview/services/IAuthService.java
@@ -1,10 +1,11 @@
 package com.api.pokemonreview.services;
 
+import com.api.pokemonreview.dtos.AuthResponseDTO;
 import com.api.pokemonreview.dtos.LoginDTO;
 import com.api.pokemonreview.dtos.RegisterDTO;
 
 public interface IAuthService {
-    String login(LoginDTO loginDTO);
+    AuthResponseDTO login(LoginDTO loginDTO);
 
     String register(RegisterDTO registerDTO);
 }
