e764b794cf19834db81c41aa219131bcebf99410
rikcav <henriquearau000@gmail.com>
2024-04-15 09:46:50 -0300

    refactor: removing unneeded dependencies

--- src/main/java/com/api/pokemonreview/controllers/AuthController.java
+++ src/main/java/com/api/pokemonreview/controllers/AuthController.java
@@ -3,15 +3,10 @@ package com.api.pokemonreview.controllers;
 import com.api.pokemonreview.dtos.AuthResponseDTO;
 import com.api.pokemonreview.dtos.LoginDTO;
 import com.api.pokemonreview.dtos.RegisterDTO;
-import com.api.pokemonreview.repositories.RoleRepository;
-import com.api.pokemonreview.repositories.UserRepository;
-import com.api.pokemonreview.security.JWTGenerator;
 import com.api.pokemonreview.services.IAuthService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,27 +15,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/auth/")
 public class AuthController {
-    //TODO refactor this code into controller and service classes
-    private AuthenticationManager authenticationManager;
-    private UserRepository userRepository;
-    private RoleRepository roleRepository;
-    private PasswordEncoder passwordEncoder;
-    private JWTGenerator jwtGenerator;
-    private IAuthService authService;
+    private final IAuthService authService;
 
     @Autowired
-    public AuthController(
-            AuthenticationManager authenticationManager,
-            UserRepository userRepository,
-            RoleRepository roleRepository,
-            PasswordEncoder passwordEncoder,
-            JWTGenerator jwtGenerator,
-            IAuthService authService) {
-        this.authenticationManager = authenticationManager;
-        this.userRepository = userRepository;
-        this.roleRepository = roleRepository;
-        this.passwordEncoder = passwordEncoder;
-        this.jwtGenerator = jwtGenerator;
+    public AuthController(IAuthService authService) {
         this.authService = authService;
     }
 
